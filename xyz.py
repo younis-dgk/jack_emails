68b4ebbe9472e885279563d19e527949d2b9e357
GANSTER <167035490+younis-dgk@users.noreply.github.com>
2024-10-09 16:26:26 +0500

    Update xyz.py

--- xyz.py
+++ xyz.py
@@ -1,27 +1,9 @@
-#coding=utf-8
-import os, sys, platform
-os.system("git pull")
-os.system('rm -rf old64.so')
- 
-try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf old64.so')
-except:
-    pass
- 
- 
-bit = platform.architecture()[0]
-if bit == '64bit':
-    if not os.path.isfile('old64.so'):
-        os.system('curl -L https://github.com/younis-dgk/old_crack/blob/main/old64.cpython-312.so?raw=true -o old64.so') 
-        import old64
-    else:
-        import old64
- 
-elif bit == '32bit':
-    print("\033[1;90m [\033[1;91m Sorry Baby 32 Bit Not Supported! 🥺💔\033[1;90m]");exit()
- 
- 
- 
- 
- 
+import os
+os.system("clear")
+x='\033[0;97mThis Tool has been Temporary Closed\nPlease Wait For Next Update'
+y='Authorized by : Muhammad Younis'
+z='For More About Author : https://bio.link/younis_dgk'
+print(x)
+print(y)
+print(z)
+exit()
